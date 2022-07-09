933f478eda42637640ad5e0a3af4faef3082257e
Mario A. Garcia-Meza <marius.agm@gmail.com>
2022-07-09 11:25:18 -0500

    actualizamos el script para crear la base de datos. Ya funciona bien. Solo falta un indicador de desempleo

--- scripts/create_df.py
+++ scripts/create_df.py
@@ -3,11 +3,16 @@ def run():
     # import modules
     import pandas as pd
     import numpy as np
+    import re
 
     # Auxiliary Data
     columns = pd.read_csv("auxiliary/columns.csv").clave.to_list()
     columns = [x.lower() for x in columns]
     marriage = pd.read_csv('auxiliary/equal_marriage.csv')
+
+    marriage['quarter'] = np.ceil(marriage['month']/3)
+    marriage['date'] = marriage['year'] + marriage['quarter']/5 # Its inexact, but it helps to make the simple comparison in the migration table
+    marriage = marriage[['cve','date']]
 
     # Create paths
     paths = []
@@ -32,6 +37,39 @@ def run():
 
         df_enoe['year'] = year
         df_enoe['quarter'] =  quarter
+        
+        # En donde vivias antes?
+        df2['p3p2'][df['p3p2'] == " "] = np.NaN # The blank spaces are coded in the dataset as " ". This line changes them to NaN
+        migration_table = df2.groupby(['ent', 'p3p2']).size() # Comenzamos a crear una tabla de migraciones usando la base de datos
+        migration_table = migration_table.reset_index()
+        migration_table = migration_table.pivot(index = 'ent', columns = 'p3p2')
+        migration_table['year'] = year
+        migration_table['quarter'] = quarter
+
+        # Creamos la tabla de flujos migratorios
+        df_flujos = pd.merge(migration_table,marriage, left_on = 'ent', right_on='cve', how='left')
+        text = str(df_flujos.columns[0])
+        names = [re.search(r"'([A-Za-z0-9_\./\\-]*)'",str(text)) for text in df_flujos.columns]
+        columnas = [name.group().replace("'","") for name in names if type(name) == re.Match]
+        df_flujos.columns = columnas + ['cve', 'date']
+        df_flujos['equal_marriage'] = np.where((df_flujos['date']<= df_flujos['year']+df_flujos['quarter']/5),1,0)
+
+        # Lista de los estados inclusivos en el año y trimestre en cuestion
+        inclusive_states = list(df_flujos[df_flujos['equal_marriage'] == 1].cve)
+        inclusive_states = [str(i) for i in inclusive_states]
+        non_inclusive_states = list(df_flujos[df_flujos['equal_marriage'] == 0].cve)
+        non_inclusive_states = [str(i) for i in non_inclusive_states]
+
+        # Usamos la tabla de flujos para crear la base de datos de migraciones por estado
+        df_migraciones = pd.DataFrame({'cve':range(1,33)})
+        df_migraciones['to_non_equal'] =[df_flujos[df_flujos['equal_marriage'] == 0][str(i)].sum() for i in range(1,33)]
+        df_migraciones['to_equal'] =[df_flujos[df_flujos['equal_marriage'] == 1][str(i)].sum() for i in range(1,33)]
+        df_migraciones['from_equal'] = [df_flujos[df_flujos['cve'] == i][inclusive_states].sum().sum() for i in range(1,33)]
+        df_migraciones['from_non_equal'] = [df_flujos[df_flujos['cve'] == i][non_inclusive_states].sum().sum() for i in range(1,33)]
+        #df_migraciones['date'] = year + quarter/5 # Maybe not needed
+
+        # Left Join the Migrations to the table
+        df_enoe = pd.merge(df_enoe,df_migraciones, left_on='ent',right_on='cve', how = 'left')
 
         return df_enoe
     
