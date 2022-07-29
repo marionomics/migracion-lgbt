20abbe4bcff396c1063f59f73d7265dc001a3db7
Mario García Meza <marius.agm@gmail.com>
2022-07-29 14:09:35 -0500

    updated did models

--- r/models/did.R
+++ r/models/did.R
@@ -1,15 +1,15 @@
 source("r/transform_database.R")
 
-names(df3) <- c("ent", "year", "migr", "equal_marriage",
+names(df4) <- c("ent", "year", "migr", "equal_marriage",
                 "from_equal", "from_non_equal", "unempl", "income", "lgbt",
-                "discrim", "dm_migr", "dm_unemp", "dm_income")
+                "discrim", "dm_migr", "dm_unemp", "dm_income", "Estado", "Vivienda")
 
-df3 <- df3 %>%
+df4 <- df4 %>%
     mutate(covid = ifelse(year %in% c(2020,2021), 1, 0))
 
 dd_model_from_equal <- lm_robust(from_equal ~ equal_marriage + dm_unemp + 
-            lgbt + discrim + dm_migr + covid,
-            data = df3,
+            lgbt + discrim + dm_migr + covid + Vivienda,
+            data = df4,
             weights = ent,
             clusters = equal_marriage
 )
@@ -18,14 +18,9 @@ summary(dd_model_from_equal)
 
 
 dd_model_from_non_equal <- lm_robust(from_non_equal ~ equal_marriage + dm_unemp + 
-            lgbt + discrim + dm_migr,
-            data = df3,
+            lgbt + discrim + dm_migr + covid + Vivienda,
+            data = df4,
             weights = ent,
             clusters = equal_marriage
 )
 summary(dd_model_from_non_equal)
-
-stargazer::stargazer(dd_model_from_equal,
- dd_model_from_non_equal)
-
-dd_model_from_equal[2]
